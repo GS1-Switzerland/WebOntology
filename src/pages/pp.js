11c88f5bc733d7bd2068362d3d79b90a301942d5
Ben-GS1 <beh@gs1.ch>
2026-07-31 15:33:10 +0200

    Delete src/pages directory

--- src/pages/pp.js
+++
@@ -1,2 +0,0 @@
-const ontologyJSONLD = "https://raw.githubusercontent.com/gs1-germany/gs1GermanyWebVoc/refs/heads/main/stagingVersion/gs1DEWebVoc.jsonld"
-const prefLang = "en"
--- src/pages/stage.js
+++
@@ -1,2 +0,0 @@
-const ontologyJSONLD = "https://raw.githubusercontent.com/gs1-germany/gs1GermanyWebVoc/refs/heads/main/currentVersion/gs1DEWebVoc.jsonld"
-const prefLang = "en"
